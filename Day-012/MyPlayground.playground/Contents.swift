b8ead1ed9bd05316caff8fdc9b659f230d199256
Amo <Akameow@icloud.com>
2026-03-14 18:39:06 +0200

    day 12 (part 1): intro to classes and inheritance

--- Day-012/MyPlayground.playground/Contents.swift
+++ Day-012/MyPlayground.playground/Contents.swift
@@ -1,3 +1,106 @@
-import Cocoa
+import Foundation
 
 var greeting = "Hello, playground"
+
+class Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var newGame = Game()
+newGame.score += 10
+
+class Employee {
+    let hours: Int
+
+    init(hours: Int) {
+        self.hours = hours
+    }
+    func printSummary() {
+        print("I work \(hours) hours a day.")
+    }
+}
+
+class Developer: Employee {
+    func work() {
+        print("I'm writing code for \(hours) hours.")
+    }
+    override func printSummary() {
+        print("I'm a developer who will sometimes work \(hours) hours a day, but other times spend hours arguing about whether code should be indented using tabs or spaces.")
+    }
+}
+
+class Manager: Employee {
+    func work() {
+        print("I'm going to meetings for \(hours) hours.")
+    }
+}
+
+let robert = Developer(hours: 8)
+let joseph = Manager(hours: 10)
+robert.work()
+joseph.work()
+
+let novall = Developer(hours: 8)
+novall.printSummary()
+
+class Vehicle {
+    let isElectric: Bool
+
+    init(isElectric: Bool) {
+        self.isElectric = isElectric
+    }
+}
+
+class Car: Vehicle {
+    let isConvertible: Bool
+
+    init(isElectric: Bool, isConvertible: Bool) {
+        self.isConvertible = isConvertible
+        super.init(isElectric: isElectric)
+    }
+}
+
+let teslaX = Car(isElectric: true, isConvertible: false)
+
+
+// ефективний приклад
+//class Car: Vehicle {
+//    let isConvertible = false
+//}
+//
+//let teslaX = Car(isElectric: true)
+
+class Vehicle2 {
+    var wheels: Int
+    init(wheels: Int) {
+        self.wheels = wheels
+    }
+}
+class Truck: Vehicle2 {
+    var goodsCapacity: Int
+    init(wheels: Int, goodsCapacity: Int) {
+        self.goodsCapacity = goodsCapacity
+        super.init(wheels: wheels)
+    }
+}
+
+//class Computer {
+  //  var cpu: String
+    //var ramGB: Int
+    //init(cpu: String, ramGB: Int) {
+      //  cpu = cpu
+    //    ramGB = ramGB
+  //  }
+//}
+//class Laptop: Computer {
+  //  var screenInches: Int
+    //init(screenInches: Int, cpu: String, ramGB: //Int) {
+        //self.screenInches = screenInches
+        //super.init(cpu: cpu, ramGB: ramGB)
+  //  }
+//}
+
